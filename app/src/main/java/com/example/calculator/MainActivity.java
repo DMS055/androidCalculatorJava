f36215bb57cbf944dfc4a69b0229be7af18792fa
passagenoir <60157796+DMS055@users.noreply.github.com>
2023-02-27 23:46:33 +0100

    Style Changes

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.calculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
@@ -19,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     TextView resultsTV;
 
     String workings = "";
+    String formula = "";
+    String tempFormula = "";
+    boolean leftBracket = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,29 +43,79 @@ public class MainActivity extends AppCompatActivity {
     public void equalsOnClick(View view) {
         Double result = null;
         ScriptEngine engine = new ScriptEngineManager().getEngineByName("rhino");
+        checkForPowerOf();
 
         try {
-            result = (double)engine.eval(workings);
+            result = (double) engine.eval(formula);
         } catch (ScriptException e) {
             Toast.makeText(this, "Invalid Input", Toast.LENGTH_SHORT).show();
         }
 
-        if (result != null) {
+        if (result != null)
             resultsTV.setText(String.valueOf(result.doubleValue()));
+
+    }
+
+    private void checkForPowerOf() {
+        ArrayList<Integer> indexOfPowers = new ArrayList<>();
+        for (int i = 0; i < workings.length(); i++) {
+            if (workings.charAt(i) == '^')
+                indexOfPowers.add(i);
         }
+
+        formula = workings;
+        tempFormula = workings;
+        for (Integer index : indexOfPowers) {
+            changeFormula(index);
+        }
+        formula = tempFormula;
+    }
+
+    private void changeFormula(Integer index) {
+        String numberLeft = "";
+        String numberRight = "";
+
+        for (int i = index + 1; i < workings.length(); i++) {
+            if (isNumeric(workings.charAt(i)))
+                numberRight = numberRight + workings.charAt(i);
+            else
+                break;
+        }
+
+        for (int i = index - 1; i >= 0; i--) {
+            if (isNumeric(workings.charAt(i)))
+                numberLeft = numberLeft + workings.charAt(i);
+            else
+                break;
+        }
+
+        StringBuilder leftNumberBuilder = new StringBuilder(numberLeft);
+        numberLeft = leftNumberBuilder.reverse().toString();
+
+        String original = numberLeft + "^" + numberRight;
+        String changed = "Math.pow(" + numberLeft + "," + numberRight + ")";
+        tempFormula = tempFormula.replace(original, changed);
+    }
+
+    private boolean isNumeric(char c) {
+        return (c <= '9' && c >= '0') || c == '.';
     }
 
     public void clearOnClick(View view) {
         workingsTV.setText("");
         workings = "";
         resultsTV.setText("");
+        leftBracket = true;
     }
 
     public void bracketsOnClick(View view) {
-    }
-
-    public void powerOnClick(View view) {
-        setWorkings("^");
+        if (leftBracket) {
+            setWorkings("(");
+            leftBracket = false;
+        } else {
+            setWorkings(")");
+            leftBracket = true;
+        }
     }
 
     public void divisionOnClick(View view) {
@@ -123,5 +176,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void zeroOnClick(View view) {
         setWorkings("0");
+    }
+
+    public void powerOnClick(View view) {
+        setWorkings("^");
     }
 }
